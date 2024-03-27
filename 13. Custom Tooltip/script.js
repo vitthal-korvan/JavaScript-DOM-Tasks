3499e25dbeb6eb99327ed9d2377d78b89ffa7e8b
vitthal-korvan <korvanvitthal@gmail.com>
2024-03-27 19:23:58 +0530

    Update script.js

--- 13. Custom Tooltip/script.js
+++ 13. Custom Tooltip/script.js
@@ -7,3 +7,5 @@ document.querySelector("#hide").addEventListener("mouseenter",function(){
 document.querySelector("#hide").addEventListener("mouseleave",function(){
   document.querySelector(".tooltip").style.display="none";
 })
+
+
